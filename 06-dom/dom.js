4b434fea9414d305f377ee338ea1fe19a2eb9788
Michaela Bačíková <bacikova.michaela@gmail.com>
2022-06-09 09:30:56 +0200

    js-api - arrays filter

---
+++ 06-dom/dom.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function upperCase() {
+  let elem = document.getElementById("fname");
+  console.log("You wrote: " + elem.value + ", type: " + elem.type);
+  document.getElementById("fname").value = elem.value.toUpperCase();
+}
+
+function youWon() {
+  let elem = document.getElementById("personForm");
+  // elem.innerHTML = `
+  // <h1> Thank you for submitting! </h1>
+  // `;
+  elem.innerHTML += `
+  <h1> Thank you for submitting! </h1>
+  `;
+}
+
+function validate() {
+  let fname = document.personForm.fname.value;
+  let color = document.personForm.color?.value;
+  console.log(fname, color);
+}
